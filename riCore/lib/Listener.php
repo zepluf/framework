75d3c31d0942eedfc6a5079335428563de664eda
yellow1912 <yellow1912@gmail.com>
2012-05-20 23:54:20 +0700

    moved folders in installation folder up 1 level

--- riCore/lib/Listener.php
+++ riCore/lib/Listener.php
@@ -8,12 +8,5 @@ use plugins\riPlugin\Plugin;
 
 class Listener
 {
-    // ...
-    public function onPageEnd(Event $event)
-    {
-    	$content = &$event->getContent();
-    	Plugin::get('riCjLoader.Loader')->injectAssets($content);
-        // extend here the functionality of the core
-        // ...
-    }
+   
 }
