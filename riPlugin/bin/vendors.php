439a40db93d54b4fcddce150a090074c89b7dbae
yellow1912 <yellow1912@gmail.com>
2012-06-03 01:39:38 +0700

    putting all modules into vendors.php

--- riPlugin/bin/vendors.php
+++ riPlugin/bin/vendors.php
@@ -16,7 +16,27 @@ $vendorDeps = array(
         array('riCjLoader', 'git@github.com:yellow1912/cjloader.git', 'origin/ZePLUF', false),
 		array('riCategory', 'git@github.com:yellow1912/riCategory.git', 'origin/master', false), 
 		array('riCache', 'git@github.com:yellow1912/riCache.git', 'origin/HEAD', false),
-    )
+    ),
+	$baseDir . '/../zenmagick/lib/base' => array(
+	    array('classloader', 'git@github.com:yellow1912/classloader.git', 'origin/HEAD', false)
+	),
+	$baseDir . '/../zenmagick/vendor/symfony/src/Symfony/Component' => array(
+		array('ClassLoader', 'git://github.com/symfony/ClassLoader.git', 'origin/HEAD', false),
+		array('DependencyInjection', 'git://github.com/symfony/DependencyInjection.git', 'origin/HEAD', false),
+		array('Config', 'git://github.com/symfony/Config.git', 'origin/HEAD', false),
+		array('Yaml', 'git://github.com/symfony/Yaml.git', 'origin/HEAD', false),
+		array('Routing', 'git://github.com/symfony/Routing.git', 'origin/HEAD', false),
+		array('HttpFoundation', 'git://github.com/symfony/HttpFoundation.git', 'origin/HEAD', false),
+		array('EventDispatcher', 'git://github.com/symfony/EventDispatcher.git', 'origin/HEAD', false),
+		array('HttpKernel', 'git://github.com/symfony/HttpKernel.git', 'origin/HEAD', false),
+		array('Validator', 'git://github.com/symfony/Validator.git', 'origin/HEAD', false),
+		array('Translation', 'git://github.com/symfony/Translation.git', 'origin/HEAD', false),
+		array('Templating', 'git://github.com/symfony/Templating.git', 'origin/HEAD', false),
+		array('Finder', 'git://github.com/symfony/Finder.git', 'origin/HEAD', false)		
+	),
+	$baseDir . '/../zenmagick/vendor/symfony/src/Symfony/Bundle' => array(
+	    array('TwigBundle', 'git://github.com/symfony/TwigBundle.git', 'origin/HEAD', false)
+	)
 );
 
 if (isset($extraDeps)) $vendorDeps = array_merge($vendorDeps, (array)$extraDeps);
