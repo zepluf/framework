a38f7d47b2dd1eeef360fc61998c4fbf03524b17
yellow1912 <yellow1912@gmail.com>
2012-07-02 14:11:14 +0700

    add some missing files

---
+++ install/zencart/includes/modules/pages/ri/header_php.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by RubikIntegration Team.
+ * User: vunguyen
+ * Date: 6/28/12
+ * Time: 10:04 AM
+ * Question? Come to our website at http://rubikintegration.com
+ */
---
+++ install/zencart/includes/templates/YOUR_TEMPLATE/templates/tpl_ri_default.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by RubikIntegration Team.
+ * User: vunguyen
+ * Date: 6/28/12
+ * Time: 10:05 AM
+ * Question? Come to our website at http://rubikintegration.com
+ */
+
+echo $response->getContent();
