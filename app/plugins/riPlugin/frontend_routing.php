afd106550a3ed7fa6b676fad84ab6e0e01dc82bf
Raine Nguyen <vunguyen@rubikin.com>
2013-01-07 16:05:07 +0700

    update for routing to work with default zencart

--- app/plugins/riPlugin/frontend_routing.php
+++ app/plugins/riPlugin/frontend_routing.php
@@ -7,27 +7,23 @@
  * Question? Come to our website at http://rubikin.com
  */
 
-if(strpos($request->getUri(), "index.php") === false) {
+if (strpos($request->getUri(), "index.php") === false) {
     // if this is not a real page, then we will attempt to use or ZePLUF MVC and sees if any controller is being mapped to it
+    $path_info = $request->getPathInfo();
+    if (!empty($path_info)) {
+        try {
+            $match = $container->get("router")->getMatcher()->match($path_info);
+            foreach ($match as $key => $value) {
+                if (strpos($key, "parameter_") !== false) {
+                    $_key = substr($key, 10);
+                    $_GET[$_key] = $_REQUEST[$_key] = $value;
+                }
+            }
+        } catch (Exception $e) {
 
-    // first we need to load our framework
-    // try to see if any controller is mapped and work okie
-    // TODO: will need print out error messages
-    try {
-        global $current_page, $current_page_base;
-        $response = $container->get("http_kernel")->handle($request);
-
-        if ($response instanceof \Symfony\Component\HttpFoundation\RedirectResponse) {
-            $container->get("http_kernel")->send();
-            $container->get("http_kernel")->terminate($request, $response);
         }
-
-        $_GET['main_page'] = $current_page = $current_page_base = 'ri';
-    } catch (\Exception $e) {
-        zen_redirect(zen_href_link('page_not_found'));
     }
-}
-else {
+} else {
     if (!is_dir(DIR_WS_MODULES . 'pages/' . $current_page)) {
 
         // first we need to load our framework
--- web/frontend_index.php
+++ web/frontend_index.php
@@ -18,16 +18,18 @@ require(__DIR__ . '/../../includes/application_top.php');
 
 // set current page
 $current_main_page = $_GET['main_page'];
-if($current_main_page == 'index'){
-    if(isset($_GET['cPath']))
+if ($current_main_page == 'index') {
+    if (isset($_GET['cPath'])) {
         $current_main_page = 'category';
-    elseif(isset($_GET['manufacturers_id']))
+    } elseif (isset($_GET['manufacturers_id'])) {
         $current_main_page = 'manufacturer';
+    }
 }
 
-$zepluf_theme = $container->get('settings')->get('theme.is_zepluf_theme', false);
-$core_event = $container->get('StoreBundle.CoreEvent');
+$zepluf_theme = $view["settings"]->get('theme.is_zepluf_theme', false);
+
 $container->get('event_dispatcher')->dispatch(Zepluf\Bundle\StoreBundle\Events::onPageStart, $core_event);
+
 ob_start();
 // eof ri: ZePLUF
 $language_page_directory = DIR_WS_LANGUAGES . $_SESSION['language'] . '/';
@@ -48,8 +50,8 @@ foreach ($directory_array as $value) {
  */
 // bof ri: ZePLUF
 // we will not load the html_header for ZePLUF theme here
-if(!$zepluf_theme){
-    require($template->get_template_dir('html_header.php',DIR_WS_TEMPLATE, $current_page_base,'common'). '/html_header.php');
+if (!$zepluf_theme) {
+    require($template->get_template_dir('html_header.php', DIR_WS_TEMPLATE, $current_page_base, 'common') . '/html_header.php');
 }
 // eof ri: ZePLUF
 /**
@@ -57,7 +59,7 @@ if(!$zepluf_theme){
  * includes/pages/{page_name}/directory to overide. Allowing different pages to have different overall
  * templates.
  */
-require($template->get_template_dir('main_template_vars.php',DIR_WS_TEMPLATE, $current_page_base,'common'). '/main_template_vars.php');
+require($template->get_template_dir('main_template_vars.php', DIR_WS_TEMPLATE, $current_page_base, 'common') . '/main_template_vars.php');
 /**
  * Read the "on_load" scripts for the individual page, and from the site-wide template settings
  * NOTE: on_load_*.js files must contain just the raw code to be inserted in the <body> tag in the on_load="" parameter.
@@ -66,9 +68,9 @@ require($template->get_template_dir('main_template_vars.php',DIR_WS_TEMPLATE, $c
 $directory_array = $template->get_template_part(DIR_WS_MODULES . 'pages/' . $current_page_base, '/^on_load_/', '.js');
 foreach ($directory_array as $value) {
     $onload_file = DIR_WS_MODULES . 'pages/' . $current_page_base . '/' . $value;
-    $read_contents='';
+    $read_contents = '';
     $lines = @file($onload_file);
-    foreach($lines as $line) {
+    foreach ($lines as $line) {
         $read_contents .= $line;
     }
     $za_onload_array[] = $read_contents;
@@ -76,30 +78,30 @@ foreach ($directory_array as $value) {
 /**
  * now read "includes/templates/TEMPLATE/jscript/on_load/on_load_*.js", which would be site-wide settings
  */
-$directory_array=array();
-$tpl_dir=$template->get_template_dir('.js', DIR_WS_TEMPLATE, 'jscript/on_load', 'jscript/on_load_');
-$directory_array = $template->get_template_part($tpl_dir ,'/^on_load_/', '.js');
+$directory_array = array();
+$tpl_dir = $template->get_template_dir('.js', DIR_WS_TEMPLATE, 'jscript/on_load', 'jscript/on_load_');
+$directory_array = $template->get_template_part($tpl_dir, '/^on_load_/', '.js');
 foreach ($directory_array as $value) {
     $onload_file = $tpl_dir . '/' . $value;
-    $read_contents='';
+    $read_contents = '';
     $lines = @file($onload_file);
-    foreach($lines as $line) {
+    foreach ($lines as $line) {
         $read_contents .= $line;
     }
     $za_onload_array[] = $read_contents;
 }
 
 // set $zc_first_field for backwards compatibility with previous version usage of this var
-if (isset($zc_first_field) && $zc_first_field !='') $za_onload_array[] = $zc_first_field;
+if (isset($zc_first_field) && $zc_first_field != '') $za_onload_array[] = $zc_first_field;
 
 $zv_onload = "";
-if (isset($za_onload_array) && count($za_onload_array)>0) $zv_onload=implode(';',$za_onload_array);
+if (isset($za_onload_array) && count($za_onload_array) > 0) $zv_onload = implode(';', $za_onload_array);
 
 //ensure we have just one ';' between each, and at the end
-$zv_onload = str_replace(';;',';',$zv_onload.';');
+$zv_onload = str_replace(';;', ';', $zv_onload . ';');
 
 // ensure that a blank list is truly blank and thus ignored.
-if (trim($zv_onload) == ';') $zv_onload='';
+if (trim($zv_onload) == ';') $zv_onload = '';
 /**
  * Define the template that will govern the overall page layout, can be done on a page by page basis
  * or using a default template. The default template installed will be a standard 3 column layout. This
@@ -108,12 +110,11 @@ if (trim($zv_onload) == ';') $zv_onload='';
 
 // bof ri: ZePLUF
 // we need to load the layout here
-if($zepluf_theme){
-    $layout = $container->get('settings')->get('theme.layouts.' . $current_main_page, 'default.php');
-    require($template->get_template_dir($layout,DIR_WS_TEMPLATE, $current_page_base,'layouts'). '/' . $layout);
-}
-else{
-    require($template->get_template_dir('tpl_main_page.php',DIR_WS_TEMPLATE, $current_page_base,'common'). '/tpl_main_page.php');
+if ($zepluf_theme) {
+    $layout = $view["settings"]->get('theme.layouts.' . $current_main_page, 'default.php');
+    require($template->get_template_dir($layout, DIR_WS_TEMPLATE, $current_page_base, 'layouts') . '/' . $layout);
+} else {
+    require($template->get_template_dir('tpl_main_page.php', DIR_WS_TEMPLATE, $current_page_base, 'common') . '/tpl_main_page.php');
     echo "</html>";
 }
 // eof ri: ZePLUF
