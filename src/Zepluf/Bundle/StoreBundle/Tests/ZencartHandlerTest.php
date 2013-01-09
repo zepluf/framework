a0ff8942808792948809b3724ae9d4dd15368b10
Tuan Nguyen <tuannguyen@rubikin.com>
2013-01-09 16:18:15 +0700

    update unit test

---
+++ src/Zepluf/Bundle/StoreBundle/Tests/ZencartHandlerTest.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Tuan Nguyen
+ * Date: 1/9/13
+ * Time: 2:12 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Zepluf\Bundle\StoreBundle\Tests;
+
+use Monolog\Logger;
+
+class ZencartHandlerTest extends BaseTestCase
+{
+    public $object;
+
+    public function setUp()
+    {
+        $this->object = $this->get('storebundle.logger.zencart_handler');
+    }
+
+    public function testAddBackEnd()
+    {
+        global $messageStack;
+        $messageStack = $this->getMock('messageStack', array('add_session', 'add'));
+
+        $record["context"]["class"] = 'messageStackError';
+        $record['message'] = 'error message';
+        $type = 'error';
+
+        $this->get('environment')->setSubEnvironment('backend');
+        //Case #1
+        $record["context"]["session"] = true;
+
+        $messageStack->expects($this->once())
+            ->method('add_session')
+            ->with($this->equalTo('error message'), $this->equalTo('error'));
+
+        $this->object->add($record, $type);
+
+        //Case #2
+        $record["context"]["session"] = false;
+
+        $messageStack->expects($this->once())
+            ->method('add')
+            ->with($this->equalTo('error message'), $this->equalTo('error'));
+
+        $this->object->add($record, $type);
+    }
+
+    public function testAddFrontEnd()
+    {
+        global $messageStack;
+        $messageStack = $this->getMock('messageStack', array('add_session', 'add'));
+
+        $record["context"]["class"] = 'messageStackError';
+        $record['message'] = 'error message';
+        $type = 'error';
+
+        $this->get('environment')->setSubEnvironment('frontend');
+
+        //Case #3
+        $record["context"]["session"] = true;
+
+        $messageStack->expects($this->once())
+            ->method('add_session')
+            ->with($this->equalTo('messageStackError'), $this->equalTo('error message'), $this->equalTo('error'));
+
+        $this->object->add($record, $type);
+
+        //Case #4
+        $record["context"]["session"] = false;
+
+        $messageStack->expects($this->once())
+            ->method('add')
+            ->with($this->equalTo('messageStackError'), $this->equalTo('error message'), $this->equalTo('error'));
+
+        $this->object->add($record, $type);
+    }
+
+    public function testWriteBackEnd()
+    {
+        $this->get('environment')->setSubEnvironment('backend');
+
+        global $messageStack;
+        $messageStack = $this->getMock('messageStack', array('add_session', 'add'));
+
+        $record["context"]["zencart"] = true;
+        $record["context"]["session"] = true;
+        $record["level"] = Logger::INFO;
+        $record["context"]["class"] = 'messageStackSuccess';
+        $record['message'] = 'success message';
+
+        $messageStack->expects($this->once())
+            ->method('add_session')
+            ->with($this->equalTo('success message'), $this->equalTo('success'));
+
+        $this->object->write($record);
+    }
+
+    public function testWriteFrontEnd()
+    {
+        $this->get('environment')->setSubEnvironment('frontend');
+
+        global $messageStack;
+        $messageStack = $this->getMock('messageStack', array('add_session', 'add'));
+
+        $record["context"]["zencart"] = true;
+        $record["context"]["session"] = false;
+        $record["level"] = Logger::INFO;
+        $record["context"]["class"] = 'messageStackSuccess';
+        $record['message'] = 'success message';
+
+        $messageStack->expects($this->once())
+            ->method('add')
+            ->with($this->equalTo('messageStackSuccess'), $this->equalTo('success message'), $this->equalTo('success'));
+
+        $this->object->write($record);
+    }
+
+    public function tearDown()
+    {
+        unset($this->object);
+    }
+}
