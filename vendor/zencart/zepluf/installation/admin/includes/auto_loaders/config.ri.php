1653ea6d6e8f52f6a7dfdbb47c5a479ebaa8b211
Raine Nguyen <vunguyen@rubikin.com>
2013-01-28 17:33:47 +0700

    remove zencart from vendor folder

--- vendor/zencart/zepluf/installation/admin/includes/auto_loaders/config.ri.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require(__DIR__.'/../../../zepluf/app/bootstrap.php');
-
-$autoLoadConfig[200][] = array(
-    'autoType' => 'require',
-    'loadFile' => $container->getParameter('kernel.root_dir') . '/init_includes.php'
-);
--- vendor/zencart/zepluf/installation/includes/auto_loaders/paypal_ipn.ri.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-require(__DIR__.'/../../zepluf/app/bootstrap.php');
-
